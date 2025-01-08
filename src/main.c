7956eee36fd5421f746a1dd8151767407be18014
Sven Schreiber <sven-schreiber@outlook.de>
2025-01-08 14:27:53 +0100

    custom camera movement

--- src/main.c
+++ src/main.c
@@ -1,5 +1,36 @@
 #include "raylib.h"
 #include "raymath.h"
+#include "rcamera.h"
+
+// Defaults from rcamera.h
+#define CAMERA_MOVE_SPEED                 5.4f       // Units per second
+#define CAMERA_ROTATION_SPEED             0.03f
+#define CAMERA_PAN_SPEED                  0.2f
+#define CAMERA_MOUSE_MOVE_SENSITIVITY     0.003f
+
+void update_camera_custom(Camera3D *camera) {
+    Vector2 mousePositionDelta = GetMouseDelta();
+
+    bool moveInWorldPlane = true;
+    bool rotateAroundTarget = true;
+    bool lockView = true;
+    bool rotateUp = false;
+
+    float cameraMoveSpeed = CAMERA_MOVE_SPEED*GetFrameTime();
+    float cameraRotationSpeed = CAMERA_ROTATION_SPEED*GetFrameTime();
+    float cameraPanSpeed = CAMERA_PAN_SPEED*GetFrameTime();
+
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+        CameraYaw(camera, -mousePositionDelta.x*CAMERA_MOUSE_MOVE_SENSITIVITY, rotateAroundTarget);
+        CameraPitch(camera, -mousePositionDelta.y*CAMERA_MOUSE_MOVE_SENSITIVITY, lockView, rotateAroundTarget, rotateUp);
+    }
+
+    if (IsKeyDown(KEY_W)) CameraMoveForward(camera, cameraMoveSpeed, moveInWorldPlane);
+    if (IsKeyDown(KEY_A)) CameraMoveRight(camera, -cameraMoveSpeed, moveInWorldPlane);
+    if (IsKeyDown(KEY_S)) CameraMoveForward(camera, -cameraMoveSpeed, moveInWorldPlane);
+    if (IsKeyDown(KEY_D)) CameraMoveRight(camera, cameraMoveSpeed, moveInWorldPlane);
+    CameraMoveToTarget(camera, -GetMouseWheelMove());
+}
 
 int main(int argc, char **argv) { 
     InitWindow(1280, 720, "HPSC Project");
@@ -16,6 +47,8 @@ int main(int argc, char **argv) {
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
+        update_camera_custom(&camera);
+
         BeginDrawing();
         ClearBackground(BLACK);
         BeginMode3D(camera);
@@ -25,6 +58,8 @@ int main(int argc, char **argv) {
         DrawGrid(10, 1.0f);
 
         EndMode3D();
+
+        DrawFPS(10, 10);
         EndDrawing();
     }
 
